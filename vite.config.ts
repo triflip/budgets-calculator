c18fe0a459fe2be97b123cfad901d4ecd32c8b7f
toni <canillask8@gmail.com>
2026-09-18 20:43:41 +0200

    chore: clean up budget components and fix pages path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/S6.Budgets/",
+  base: "/budgets-calculator/",
   plugins: [react()],
     server: {
     open: true, 
